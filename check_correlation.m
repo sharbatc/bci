75e6c38f0d422eca8764be98eaa4d5a911c838dc
András Ecker <andras.ecker@epfl.ch>
2017-05-07 19:27:06 +0200

    correlation check added

---
+++ check_correlation.m
@@ -0,0 +1,36 @@
+function [horizontal_corr, vertical_corr] =  check_correlation(channels, eye_channels, threshold)
+% checks the correlation of the channels with EOG (downsampled by 8 first)
+
+eye_movement_corr = zeros(2,64);  % store results
+left_eye = eye_channels(1,1:8:end);
+nasion = eye_channels(2,1:8:end);
+right_eye = eye_channels(3,1:8:end);
+horizontal_eye_movement = left_eye-right_eye;
+vertical_eye_movement = nasion - mean([left_eye; right_eye]);
+for i=1:64
+    chan = channels(i,1:8:end);
+    % horizontal eye movement (1st row in eye_movement_corr)
+    tmp = corrcoef(chan, horizontal_eye_movement); % 2*2 matrix
+    corr_ = tmp(1,2);
+    eye_movement_corr(1,i) = corr_;
+    % vertical eye movement (2nd row in eye_movement_corr)
+    tmp = corrcoef(chan, vertical_eye_movement); % 2*2 matrix
+    corr_ = tmp(1,2);
+    eye_movement_corr(2,i) = corr_;
+end
+
+% check for high correlation
+horizontal_corr = [];
+vertical_corr = [];
+[row, col] = find(abs(eye_movement_corr) > threshold);
+for i=1:size(row)
+    if row(i) == 1
+        horizontal_corr = [horizontal_corr, col(i)];
+        %fprintf('horizontal eye movement correlation is higher than %2f - chan:%i \n',threshold, col(i));
+    elseif row(i) == 2
+        vertical_corr = [vertical_corr, col(i)];
+        %fprintf('vertical eye movement correlation is higher than %2f chan:%i \n',threshold, col(i));
+    end
+end
+
+end
--- main.m
+++ main.m
@@ -9,8 +9,23 @@ clear all;
 %fName = '/home/bandi/EPFL/BCI/ag2_22032017.bdf'; % Andras' 1st
 %[header, channels, eye_channels, biceps_channels, cleared_trigger] = initialize(fName);
 %saveName = '/home/bandi/EPFL/BCI/Andras_1.mat';
-%trials = save_to_mat(header, channels, eye_channels, biceps_channels, cleared_trigger, saveName);
+%data = save_to_mat(header, channels, eye_channels, biceps_channels, cleared_trigger, saveName);
 
 %% load in data from saved .mat file (struct of structs)
 fName = '/home/bandi/EPFL/BCI/Andras_1.mat';
-trials = load(fName);
+load(fName);
+fprintf('data loaded!')
+
+%% check correlation
+clc;
+corr_threshold = 0.8;
+trials = {'t1', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 't9', 't10', 't11', 't12', 't13', 't14', 't15'}; % stupid MATLAB...
+discard_channels = zeros(1,64);
+for i=1:15
+    [horizontal_corr, vertical_corr] = check_correlation(data.(trials{i}).channels,...
+                                                         data.(trials{i}).eye_channels, corr_threshold);
+    discard_channels_trial = [horizontal_corr, vertical_corr];  % channels discarded according to this trial
+    discard_channels(discard_channels_trial) = discard_channels(discard_channels_trial)+1; % no += 1 :(
+end
+% channels wich have high(er than threshold) correlation in (at least) half of the trials
+discard = find(discard_channels > 7)  
