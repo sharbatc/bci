458d64baf4fc429924a9081f7d56ba2a6709b23f
Elisabetta Maria Messina <elisabettamessina@tsf-452-wpa-5-177.epfl.ch>
2017-05-23 20:34:20 +0200

    temporal filter nice for plots

--- temporal_filter.m
+++ temporal_filter.m
@@ -1,9 +1,18 @@
 function filtered_channels = temporal_filter(channels, Fs)
 % temporal filtering of the signal 
-% (girls' code replaced by András's butter filter code...)
+% (girls' code replaced by Andr??s's butter filter code...)
 
-D = designfilt('bandpassiir','FilterOrder',20,'HalfPowerFrequency1',0.001,'HalfPowerFrequency2',50,'SampleRate',Fs,'DesignMethod','butter');
+D = designfilt('bandpassiir','FilterOrder',20,'HalfPowerFrequency1',1,'HalfPowerFrequency2',40,'SampleRate',Fs,'DesignMethod','butter');
 
-filtered_channels = filter(D,channels);
+filtered_channels = filter(D,channels');
+filtered_channels = filtered_channels';
+
+%compare signals:
+%pwelch(channels(1,:),[],[],[],Fs)
+%pwelch(filtererd_channels(1,:),[],[],[],Fs)
+
+%display the filter:
+%fvtool(D,'MagnitudeDisplay','zero-phase','Fs',Fs)
+
 
 end
