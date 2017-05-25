acb687b4e2bb015f93d63191cdde40bd5bc39c00
András Ecker <andras.ecker@epfl.ch>
2017-05-25 17:49:35 +0200

    cutted at 1Hz

--- temporal_filter.m
+++ temporal_filter.m
@@ -1,18 +1,10 @@
 function filtered_channels = temporal_filter(channels, Fs)
 % temporal filtering of the signal 
-% (girls' code replaced by Andr??s's butter filter code...)
+% (girls' code replaced by Andras's butter filter code...)
 
-filtered_channels = filter(D,channels');
-filtered_channels = filtered_channels';
-
-%compare signals:
-%pwelch(channels(1,:),[],[],[],Fs)
-%pwelch(filtererd_channels(1,:),[],[],[],Fs)
-
+D = designfilt('bandpassiir','FilterOrder',20,'HalfPowerFrequency1',1,'HalfPowerFrequency2',45,'SampleRate',Fs,'DesignMethod','butter');
 %display the filter:
 %fvtool(D,'MagnitudeDisplay','zero-phase','Fs',Fs)
-
-D = designfilt('bandpassiir','FilterOrder',20,'HalfPowerFrequency1',0.1,'HalfPowerFrequency2',45,'SampleRate',Fs,'DesignMethod','butter');
 
 filtered_channels = filter(D,channels);
 
