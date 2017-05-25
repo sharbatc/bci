8952daeee9009d2c079a98809c5cc71ccfb4072d
Elisabetta Maria Messina <elisabettamessina@tsf-452-wpa-5-177.epfl.ch>
2017-05-25 17:46:05 +0200

    topographic plot

--- temporal_filter.m
+++ temporal_filter.m
@@ -2,9 +2,6 @@ function filtered_channels = temporal_filter(channels, Fs)
 % temporal filtering of the signal 
 % (girls' code replaced by Andr??s's butter filter code...)
 
-filtered_channels = filter(D,channels');
-filtered_channels = filtered_channels';
-
 %compare signals:
 %pwelch(channels(1,:),[],[],[],Fs)
 %pwelch(filtererd_channels(1,:),[],[],[],Fs)
@@ -12,7 +9,7 @@ filtered_channels = filtered_channels';
 %display the filter:
 %fvtool(D,'MagnitudeDisplay','zero-phase','Fs',Fs)
 
-D = designfilt('bandpassiir','FilterOrder',20,'HalfPowerFrequency1',0.1,'HalfPowerFrequency2',45,'SampleRate',Fs,'DesignMethod','butter');
+D = designfilt('bandpassiir','FilterOrder',20,'HalfPowerFrequency1', 1,'HalfPowerFrequency2',50,'SampleRate',Fs,'DesignMethod','butter');
 
 filtered_channels = filter(D,channels);
 
