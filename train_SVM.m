620bd34241d1643802594d7aa6a1d4fe24855aca
András Ecker <andras.ecker@epfl.ch>
2017-05-31 21:11:06 +0200

    ROC plot updated

--- train_SVM.m
+++ train_SVM.m
@@ -13,13 +13,13 @@ classifier = fitSVMPosterior(SVMModel);  % to get probabilities, not only labels
 train_err = classerror(labels_train, predict(classifier, train));
 C_train = confusionmat(labels_train, predict(classifier, train));  % not used so far, but return it any way...
 test_err = classerror(labels_test, yhat);
-C_test = confusionmat(labels_test, yhat);
+C_test = confusionmat(labels_test, yhat);  % not used so far, but return it any way...
 
 [ROC_x,ROC_y,~,AUC] = perfcurve(labels_test,scores(:,2), 1);
 
 % check linearly separable data, and 3 point ROC curve
 tmp = size(labels_test,1)+1;
-if size(ROC_x,1) ~= tmp
+if size(ROC_x,1) == 3
     ROC_x = linspace(0,1,tmp)';
     ROC_y = ones(tmp,1);
     ROC_y(1) = 0;
