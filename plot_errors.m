af0b923714974adb0ff674daaa1f7ce7c69bbb5f
András Ecker <andras.ecker@epfl.ch>
2017-06-02 15:35:50 +0200

    0.1 line added

--- plot_errors.m
+++ plot_errors.m
@@ -6,6 +6,8 @@ figure;
 grp = [zeros(nfolds,1); ones(nfolds,1); 2*ones(nfolds,1); 3*ones(nfolds,1); 4*ones(nfolds,1); 5*ones(nfolds,1)];
 boxplot([linear, diaglinear, quadratic, diagquadratic, SVM, NB], grp,...
         'labels',{'linear', 'diaglinear', 'quadratic', 'diagquadratic', 'SVM', 'Naive Bayes'});
+hold on;
+plot(xlim,[0.1,0.1],'k--');
 title(sprintf('%s: %s errors (%i-fold CV)',name,mode,nfolds));
 ylabel('classerror');
 
