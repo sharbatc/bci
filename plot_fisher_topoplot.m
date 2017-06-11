07fa2859bce2be066c044a41ec44cd6318a9d996
András Ecker <andras.ecker@epfl.ch>
2017-06-11 15:11:31 +0200

    Fisher plot added

--- plot_fisher_topoplot.m
+++ plot_fisher_topoplot.m
@@ -1,13 +1,12 @@
-function plot_fisher_topoplot(labels, features, eeglab_path, ses, name)
-% plots integral of the whole power, projected to the scalp
+function plot_fisher_topoplot(labels, features, disc, eeglab_path, ses, name)
+% plots integral (easy & hard) of the whole power and fisher score, projected to the scalp
 
-addpath(sprintf('%s/functions/sigprocfunc',eeglab_path),...   
-        sprintf('%s/functions/guifunc',eeglab_path),...  
-        sprintf('%s/functions/adminfunc',eeglab_path));   
+addpath(sprintf('%s/functions/sigprocfunc',eeglab_path),...
+        sprintf('%s/functions/guifunc',eeglab_path),...
+        sprintf('%s/functions/adminfunc',eeglab_path));
 
 % get out power from feature matrix
 band_powers = features(:,end-(64*7)+1:end); % get the corresponding values from the row vector
-
 % initialize arrays to store values for every channels
 power_easy = [];
 power_hard = [];
@@ -19,38 +18,35 @@ for i=1:size(labels)
         power_easy = [power_easy, power];
     elseif labels(i) == 1
         power_hard = [power_hard, power];
-    end
-    
+    end 
 end
-
 power_easy = mean(power_easy,2);
 power_hard = mean(power_hard,2);
 
+% get out power from Fisher scores
+fisher_power = disc(:,end-6); % get the Fisher score of the integral
+fisher_power = fisher_power - mean(fisher_power);
+
 figure;
-set(gca,'fontsize',13);
-set(gcf,'units','points','position',[100,100,1000,500]);
-
-subplot(1,2,1);
 topoplot((power_easy-mean(power_easy)), 'eeglab_chan64_2.elp');
-set(gca,'fontsize',15);
+%topoplot(power_easy, 'eeglab_chan64_2.elp');
 title(sprintf('%s: power - easy trials, session:%i',name,ses));
-subplot(1,2,2);
-topoplot((power_hard-mean(power_hard)), 'eeglab_chan64_2.elp');
 set(gca,'fontsize',15);
-
-title(sprintf('%s: power - hard trials, session:%i',name,ses));
-
-fName = sprintf('pictures/s%i_%s_power.png',ses,name);
+fName = sprintf('pictures/s%i_%s_power_easy.png',ses,name);
 saveas(gcf, fName);
 
-% take the difference!
 figure;
-set(gca,'fontsize',13);
+topoplot((power_hard-mean(power_hard)), 'eeglab_chan64_2.elp');
+%topoplot(power_hard, 'eeglab_chan64_2.elp');
+title(sprintf('%s: power - hard trials, session:%i',name,ses));
+set(gca,'fontsize',15);
+fName = sprintf('pictures/s%i_%s_power_hard.png',ses,name);
+saveas(gcf, fName);
 
-topoplot((power_easy-power_hard-mean(power_easy-power_hard)), 'eeglab_chan64_2.elp');  % not Fisher score! (just the difference of the 2 prev plots)
-title(sprintf('%s: power disc. chans (easy-hard) session:%i',name,ses));
-
-
+figure;
+topoplot(fisher_power, 'eeglab_chan64_2.elp');
+title(sprintf('%s: Fisher score of power, session:%i',name,ses));
+set(gca,'fontsize',15);
 fName = sprintf('pictures/s%i_%s_power2.png',ses,name);
 saveas(gcf, fName);
 
